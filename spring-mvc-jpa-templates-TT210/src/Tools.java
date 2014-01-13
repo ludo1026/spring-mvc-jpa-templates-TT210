54bec11ab35679b6406f7522c5e170094e90a717
ludo1026 <l.chaboud@gmail.com>
2014-01-13 11:42:14 +0100

    templates

--- spring-mvc-jpa-templates-TT210/src/Tools.java
+++ spring-mvc-jpa-templates-TT210/src/Tools.java
@@ -4,19 +4,80 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.telosys.tools.commons.variables.Variable;
 import org.telosys.tools.generator.context.JavaBeanClass;
 import org.telosys.tools.generator.context.JavaBeanClassAttribute;
 import org.telosys.tools.generator.context.JavaBeanClassForeignKey;
 import org.telosys.tools.generator.context.JavaBeanClassForeignKeyColumn;
 import org.telosys.tools.generator.context.JavaBeanClassLink;
-import org.telosys.tools.generator.context.Model;
+import org.telosys.tools.generator.context.ProjectInContext;
 
 public class Tools {
+
+	/**
+	 * Get variable form project configuration. 
+	 * @param projectConfiguration Project configuration
+	 * @param variableName Variable name
+	 * @return Variable
+	 */
+	public Variable getVariable(ProjectInContext projectConfiguration, String variableName) {
+		for(Variable variable : projectConfiguration.getAllVariables()) {
+			if(variable.getName().equalsIgnoreCase(variableName)) {
+				return variable;
+			}
+		}
+		return null;
+	}
 	
 	/**
-	 * Retourne l'attribut de la clé.
-	 * @param entity Entité
-	 * @return Attribut de la clé
+	 * Get variable valeu from project configuration
+	 * @param projectConfiguration Project configuration
+	 * @param variableName Variable name
+	 * @return Variable value
+	 */
+	public String getVariableValue(ProjectInContext projectConfiguration, String variableName) {
+		Variable variable = getVariable(projectConfiguration, variableName);
+		if(variable == null) {
+			return "";
+		} else {
+			return variable.getValue();
+		}
+	}
+	
+	/**
+	 * Indicates if the attribute type is text
+	 * @param attribute Attribute
+	 * @return boolean
+	 */
+	public boolean isText(JavaBeanClassAttribute attribute) {
+		String[] types = {"String"};
+		return Arrays.asList(types).contains(attribute.getWrapperType());
+	}
+	
+	/**
+	 * Indicates if the attribute is a number
+	 * @param attribute Attribute
+	 * @return boolean
+	 */
+	public boolean isNumber(JavaBeanClassAttribute attribute) {
+		String[] types = {"Integer","Long","Double","Short","Byte","Float","Number","BigInteger","BigDecimal"};
+		return Arrays.asList(types).contains(attribute.getWrapperType());
+	}
+
+	/**
+	 * Indicates if the attribute is a date
+	 * @param attribute Attribute
+	 * @return boolean
+	 */
+	public boolean isDate(JavaBeanClassAttribute attribute) {
+		String[] types = {"Date","Calendar"};
+		return Arrays.asList(types).contains(attribute.getWrapperType());
+	}
+	
+	/**
+	 * Get entity key attribute.
+	 * @param entity Entity
+	 * @return attribute
 	 */
 	public JavaBeanClassAttribute keyField(JavaBeanClass entity) {
 		if(entity.hasCompositePrimaryKey()) {
@@ -28,9 +89,9 @@ public class Tools {
 	}
 	
 	/**
-	 * Retourne le type de la clé.
-	 * @param entity Entité
-	 * @return Type de la clé
+	 * Get entity key attribute type.
+	 * @param entity Entity
+	 * @return Attribute type
 	 */
 	public String keyType(JavaBeanClass entity) {
 		if(entity.hasCompositePrimaryKey()) {
@@ -42,9 +103,9 @@ public class Tools {
 	}
 	
 	/**
-	 * Retourne le nom de la clé.
-	 * @param entity Entité
-	 * @return Nom de la clé
+	 * Get entity key attribute name.
+	 * @param entity Entity
+	 * @return attribute name
 	 */
 	public String keyName(JavaBeanClass entity) {
 		if(entity.hasCompositePrimaryKey()) {
@@ -56,9 +117,9 @@ public class Tools {
 	}
 
 	/**
-	 * Retourne le getter de la clé.
-	 * @param entity Entité
-	 * @return Getter de la clé
+	 * Get entity key attribute getter.
+	 * @param entity Entity
+	 * @return Attribute getter
 	 */
 	public String keyGetter(JavaBeanClass entity) {
 		if(entity.hasCompositePrimaryKey()) {
@@ -70,9 +131,9 @@ public class Tools {
 	}
 
 	/**
-	 * Retourne le setter de la clé.
-	 * @param entity Entité
-	 * @return Setter de la clé
+	 * Get entity key attribute setter.
+	 * @param entity Entity
+	 * @return attribute setter.
 	 */
 	public String keySetter(JavaBeanClass entity) {
 		if(entity.hasCompositePrimaryKey()) {
@@ -84,10 +145,10 @@ public class Tools {
 	}
 	
 	/**
-	 * Indique si le champ de la clé primaire est une valeur auto-générée.
-	 * @param entity Entité
-	 * @param field Champ de la clé primaire
-	 * @return booléen
+	 * Indicates if the primary key is an auto-generated value.
+	 * @param entity Entity
+	 * @param field Key attribute
+	 * @return boolean
 	 */
 	public boolean isGeneratedValue( JavaBeanClass entity, JavaBeanClassAttribute field) {
 		boolean isGeneratedValue = false;
@@ -111,10 +172,10 @@ public class Tools {
 	}
 	
 	/**
-	 * Indique si l'entité a un champ dont la valeur est auto-générée.
-	 * @param entity Entité
-	 * @param fields Champs
-	 * @return booléen
+	 * Indicates if one of these key attributes is an auto-generated value.
+	 * @param entity Entity
+	 * @param fields Key attributes
+	 * @return boolean
 	 */
 	public boolean hasGeneratedValue( JavaBeanClass entity, List<JavaBeanClassAttribute> fields) {
 		boolean hasGeneratedValue = false;
@@ -285,4 +346,41 @@ public class Tools {
 		return fieldsMappingForLink;
 	}
 
+	/**
+	 * Get entity links selected on attribute criteria.
+	 * @param entity Entity
+	 * @param criterias Criteria
+	 * @return links
+	 */
+	public List<JavaBeanClassLink> links(JavaBeanClass entity, int... criterias) {
+		if(criterias == null || criterias.length == 0) {
+			return entity.getLinks();
+		}
+		List<JavaBeanClassAttribute> attributes = new ArrayList<JavaBeanClassAttribute>();
+		if(criterias.length == 1) {
+			attributes = entity.getAttributesByCriteria(criterias[0]);
+		} else if(criterias.length == 2) {
+			attributes = entity.getAttributesByCriteria(criterias[0], criterias[1]);
+		} else if(criterias.length == 3) {
+			attributes = entity.getAttributesByCriteria(criterias[0], criterias[1], criterias[2]);
+		} else if(criterias.length == 4) {
+			attributes = entity.getAttributesByCriteria(criterias[0], criterias[1], criterias[2], criterias[3]);
+		} 
+		// Récupérer les liens à partir des attributs
+		return linksForFields(entity.getLinks(), attributes);
+	}
+
+	/**
+	 * Get class name.
+	 * @param object Object
+	 * @return Class name
+	 */
+	public String getClassName(Object object) {
+		if(object == null) {
+			return "null";
+		} else {
+			return object.getClass().getName();
+		}
+	}
+	
 }
